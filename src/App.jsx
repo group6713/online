57a07cd7c1be0f90f70de84c9fef25adb87c0ae8
Nozimjon <masteryt123@mail.ru>
2024-09-12 21:08:08 +0500

    part created | 1-version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "./NozimjonNN.css"
+import img from "./Img/main.png"
+
 function App() {
     return (
     <>
-      salom
+      <div className="NozimjonNN">
+        <span className="N1">Elevate your <br /> SEO efforts.</span>
+        <div className="Ngroup">
+          <img src={ img } className="Nimg"/>
+        </div>
+      </div>
     </>
   )
 }
