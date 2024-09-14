91cbac74ddd92a6845e6797a70ac2d6ac4137d9a
Asad <Asad@gmail.com>
2024-09-14 20:30:01 +0500

    Asad bot

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NozimjonPart from "./Components/NozimjonNN/NozimjonPart"
-
+import "./Asadbek"
+import Asadbek from "./Components/Asadbek/Asad"
 function App() {
     return (
     <>
+    <Asadbek/>
       <NozimjonPart />
       salom
     </>
